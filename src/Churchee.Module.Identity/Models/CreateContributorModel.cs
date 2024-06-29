cd3ab827d9eb0a40b669c453de0269e74463b67b
Jon <jon@treefish.uk>
2024-06-29 21:22:22 +0100

    Added Contributors Listing and Create views

---
+++ src/Churchee.Module.Identity/Models/CreateContributorModel.cs
@@ -0,0 +1,36 @@
+﻿using Churchee.Common.ValueTypes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Churchee.Module.Identity.Models
+{
+    public class CreateContributorModel
+    {
+
+        public CreateContributorModel(MultiSelect roles)
+        {
+            Email = string.Empty;
+            Password = string.Empty;
+            ConfirmPassword = string.Empty;
+            Roles = roles;
+        }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 10)]
+        [DataType(DataTypes.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataTypes.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataTypes.CheckboxList)]
+        public MultiSelect Roles { get; set; }
+
+    }
+}
