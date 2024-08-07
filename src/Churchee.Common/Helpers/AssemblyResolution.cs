42e357943e1c5876aaf3ff0c9e83b4bf46186798
Jon <jon@treefish.uk>
2024-08-07 16:13:57 +0100

    Assembly Resolution update

--- src/Churchee.Common/Helpers/AssemblyResolution.cs
+++ src/Churchee.Common/Helpers/AssemblyResolution.cs
@@ -1,30 +1,30 @@
-﻿using System;
+﻿using Churchee.Common.Abstractions.Extensibility;
+using Microsoft.Extensions.DependencyModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Churchee.Common.Abstractions.Extensibility;
-using Microsoft.Extensions.DependencyModel;
 
-public static class AssemblyResolution
+namespace Churchee.Common.Helpers
 {
-    public static IEnumerable<Assembly> GetAssemblies()
+    public static class AssemblyResolution
     {
-        return DependencyContext.Default.RuntimeLibraries
-            .SelectMany(library => library.GetDefaultAssemblyNames(DependencyContext.Default))
-            .Where(w => w.FullName.StartsWith("Churchee"))
-            .Select(Assembly.Load)
-            .Distinct()
-            .ToList();
+        public static IEnumerable<Assembly> GetAssemblies()
+        {
+            return DependencyContext.Default.RuntimeLibraries
+                .SelectMany(library => library.GetDefaultAssemblyNames(DependencyContext.Default))
+                .Where(w => w.FullName.StartsWith("Churchee"))
+                .Select(Assembly.Load)
+                .Distinct()
+                .ToList();
+        }
+
+        public static Assembly[] GetModuleAssemblies()
+        {
+            var returnAssemblies = GetAssemblies()
+                .Where(w => w.FullName.StartsWith("Churchee") && w.GetTypes().Any(a => a.GetInterfaces().Contains(typeof(IModule))));
+
+            return returnAssemblies.ToArray();
+        }
     }
-
-    public static Assembly[] GetModuleAssemblies()
-    {
-        var returnAssemblies = GetAssemblies()
-            .Where(w => w.FullName.StartsWith("Churchee") && w.GetTypes().Any(a => a.GetInterfaces().Contains(typeof(IModule))));
-
-        return returnAssemblies.ToArray();
-    }
-
-
-
 }
