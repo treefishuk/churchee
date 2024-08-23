b1bc5bf902bdb60a2ab7d484f950a9daa4df28b8
Jon <jon@treefish.uk>
2024-08-23 16:15:09 +0100

    Added HIBP Check to Password updates

---
+++ src/Churchee.Module.Identity/Features/HIBP/Queries/CheckPasswordAgainstHIBP/CheckPasswordAgainstHIBPCommand.cs
@@ -0,0 +1,15 @@
+﻿using Churchee.Common.ResponseTypes;
+using MediatR;
+
+namespace Churchee.Module.Identity.Features.HIBP.Queries
+{
+    public class CheckPasswordAgainstHIBPCommand : IRequest<CommandResponse>
+    {
+        public CheckPasswordAgainstHIBPCommand(string password)
+        {
+            Password = password;
+        }
+
+        public string Password { get; }
+    }
+}
---
+++ src/Churchee.Module.Identity/Features/HIBP/Queries/CheckPasswordAgainstHIBP/CheckPasswordAgainstHIBPCommandHandler.cs
@@ -0,0 +1,83 @@
+﻿using Churchee.Common.ResponseTypes;
+using MediatR;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Churchee.Module.Identity.Features.HIBP.Queries
+{
+    public class CheckPasswordAgainstHIBPCommandHandler : IRequestHandler<CheckPasswordAgainstHIBPCommand, CommandResponse>
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CheckPasswordAgainstHIBPCommandHandler(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<CommandResponse> Handle(CheckPasswordAgainstHIBPCommand request, CancellationToken cancellationToken)
+        {
+
+            var commandResponse = new CommandResponse();
+
+            if (string.IsNullOrEmpty(request.Password))
+            {
+                commandResponse.AddError("No Password Provided", "Password");
+
+                return commandResponse;
+            }
+
+            try
+            {
+
+                string hash = ComputeSha1Hash(request.Password);
+                string hashPrefix = hash[..5];
+                var hashSuffix = hash.Substring(5).ToUpper();
+
+                var response = await _httpClientFactory.CreateClient().GetAsync($"https://api.pwnedpasswords.com/range/{hashPrefix}");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    commandResponse.AddError("Error Calling hibp API", "Password");
+
+                    return commandResponse;
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                var lines = content.Split("\r\n");
+
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(':');
+
+                    var suffix = parts[0];
+                    var count = int.Parse(parts[1]);
+
+                    if (hashSuffix == suffix)
+                    {
+                        commandResponse.AddError($"Password found in leaked passwords database (HIBP) {count} times!. Please try a different password, ideally generated via a password manager", "Password");
+
+                        return commandResponse;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                commandResponse.AddError("Error Calling HIBP API", "");
+            }
+
+            return commandResponse;
+        }
+
+        private string ComputeSha1Hash(string input)
+        {
+            using var sha1 = System.Security.Cryptography.SHA1.Create();
+            var hashBytes = sha1.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+    }
+}
