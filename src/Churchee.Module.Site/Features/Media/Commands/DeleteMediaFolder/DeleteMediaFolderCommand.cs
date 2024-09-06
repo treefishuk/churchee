c0ca134aaefcb160c5cbbb77bb594ed4a01715d4
Jon <jon@treefish.uk>
2024-09-06 20:47:16 +0100

    Added Delete Folder Support for media section

---
+++ src/Churchee.Module.Site/Features/Media/Commands/DeleteMediaFolder/DeleteMediaFolderCommand.cs
@@ -0,0 +1,15 @@
+﻿using Churchee.Common.ResponseTypes;
+using MediatR;
+
+namespace Churchee.Module.Site.Features.Media.Commands
+{
+    public class DeleteMediaFolderCommand : IRequest<CommandResponse>
+    {
+        public DeleteMediaFolderCommand(Guid folderId)
+        {
+            FolderId = folderId;
+        }
+
+        public Guid FolderId { get; }
+    }
+}
---
+++ src/Churchee.Module.Site/Features/Media/Commands/DeleteMediaFolder/DeleteMediaFolderCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Churchee.Common.Abstractions.Auth;
+using Churchee.Common.ResponseTypes;
+using Churchee.Common.Storage;
+using Churchee.Module.Site.Entities;
+using MediatR;
+
+namespace Churchee.Module.Site.Features.Media.Commands
+{
+    public class DeleteMediaFolderCommandHandler : IRequestHandler<DeleteMediaFolderCommand, CommandResponse>
+    {
+
+        private readonly ICurrentUser _currentUser;
+        private readonly IDataStore _dataStore;
+
+        public DeleteMediaFolderCommandHandler(ICurrentUser currentUser, IDataStore dataStore)
+        {
+            _currentUser = currentUser;
+            _dataStore = dataStore;
+        }
+
+        public async Task<CommandResponse> Handle(DeleteMediaFolderCommand request, CancellationToken cancellationToken)
+        {
+            var response = new CommandResponse();
+
+
+            try
+            {
+                var repo = _dataStore.GetRepository<MediaFolder>();
+
+                var applicationTenantId = await _currentUser.GetApplicationTenantId();
+
+                await repo.SoftDelete(request.FolderId);
+
+                await _dataStore.SaveChangesAsync(cancellationToken);
+
+            }
+            catch (Exception)
+            {
+                response.AddError("Failed to remove folder", "folderId");
+            }
+
+            return new CommandResponse();
+        }
+    }
+}
