c5329db66cad3a7c297f9e9367d183d176c2a73a
Jon Ryan <jon@treefish.uk>
2024-09-30 19:01:18 +0100

    More Tests

---
+++ test/Churchee.Module.Events.Tests/Features/Commands/ActivateEvents/ActivateEventsCommandHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using Churchee.Common.Abstractions.Storage;
+using Churchee.Common.Storage;
+using Churchee.Module.Events.Features.Commands;
+using Churchee.Module.Site.Entities;
+using Churchee.Module.Tenancy.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace Churchee.Module.Events.Tests.Features.Commands.ActivateEvents
+{
+    public class ActivateEventsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ActivateEventsCommandHandler_Handle_ReturnsCommandResponse()
+        {
+            //arrange
+            var applicationTenantId = Guid.NewGuid();
+            var request = new ActivateEventsCommand(applicationTenantId);
+            var cut = new ActivateEventsCommandHandler(GetDataStore());
+
+            //act
+            var result = await cut.Handle(request, default);
+
+            //assert
+            result.IsSuccess.Should().BeTrue();
+        }
+
+
+        private static IDataStore GetDataStore()
+        {
+            var mockApplicationTenantRepository = new Mock<IRepository<ApplicationTenant>>();
+            mockApplicationTenantRepository.Setup(s => s.GetByIdAsync(It.IsAny<object>())).ReturnsAsync(new ApplicationTenant(Guid.NewGuid(), "Test", 1));
+
+            var mockPageRepository = new Mock<IRepository<Page>>();
+            var mockPageTypeRepository = new Mock<IRepository<PageType>>();
+            var mockViewTemplateRepository = new Mock<IRepository<ViewTemplate>>();
+
+            var mockDataStore = new Mock<IDataStore>();
+
+            mockDataStore.Setup(s => s.GetRepository<ApplicationTenant>()).Returns(mockApplicationTenantRepository.Object);
+            mockDataStore.Setup(s => s.GetRepository<PageType>()).Returns(mockPageTypeRepository.Object);
+            mockDataStore.Setup(s => s.GetRepository<Page>()).Returns(mockPageRepository.Object);
+            mockDataStore.Setup(s => s.GetRepository<ViewTemplate>()).Returns(mockViewTemplateRepository.Object);
+
+            return mockDataStore.Object;
+        }
+
+    }
+}
---
+++ test/Churchee.Module.Events.Tests/Features/Commands/ActivateEvents/ActivateEventsCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using Churchee.Module.Events.Features.Commands;
+using FluentAssertions;
+
+namespace Churchee.Module.Events.Tests.Features.Commands.ActivateEvents
+{
+    public class ActivateEventsCommandTests
+    {
+        [Fact]
+        public void ActivateEventsCommand_PropertiesSetByConstructor()
+        {
+            //arrange
+            var applicationTenantId = Guid.NewGuid();
+
+            //act
+            var command = new ActivateEventsCommand(applicationTenantId);
+
+            //assert
+            command.ApplicationTenantId.Should().Be(applicationTenantId);
+        }
+    }
+}
