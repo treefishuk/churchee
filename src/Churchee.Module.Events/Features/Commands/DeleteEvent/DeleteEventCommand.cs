53681b2177b579378de4c56c3167235c435d5c0b
Jon Ryan <jon@treefish.uk>
2024-09-30 21:34:34 +0100

    DeleteEventCommandTests

--- src/Churchee.Module.Events/Features/Commands/DeleteEvent/DeleteEventCommand.cs
+++ src/Churchee.Module.Events/Features/Commands/DeleteEvent/DeleteEventCommand.cs
@@ -5,11 +5,11 @@ namespace Churchee.Module.Events.Features.Commands
 {
     public class DeleteEventCommand : IRequest<CommandResponse>
     {
-        public Guid EventId { get; set; }
-
         public DeleteEventCommand(Guid eventId)
         {
             EventId = eventId;
         }
+
+        public Guid EventId { get; }
     }
 }
---
+++ test/Churchee.Module.Events.Tests/Features/Commands/DeleteEvent/DeleteEventCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using Churchee.Module.Events.Features.Commands;
+using FluentAssertions;
+
+namespace Churchee.Module.Events.Tests.Features.Commands.DeleteEvent
+{
+    public class DeleteEventCommandTests
+    {
+        [Fact]
+        public void DeleteEventCommand_ParametersSet()
+        {
+            //arrange
+            var eventId = Guid.NewGuid();
+
+            //act
+            var cut = new DeleteEventCommand(eventId);
+
+            //assert
+            cut.EventId.Should().Be(eventId);
+        }
+
+    }
+}
