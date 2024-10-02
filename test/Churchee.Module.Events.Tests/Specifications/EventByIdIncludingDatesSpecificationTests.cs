ec8b55cd8f903e0cde14ed672c5d81e509f08800
Jon Ryan <jon@treefish.uk>
2024-10-02 22:06:28 +0100

    More Work on Events

---
+++ test/Churchee.Module.Events.Tests/Specifications/EventByIdIncludingDatesSpecificationTests.cs
@@ -0,0 +1,28 @@
+﻿using Churchee.Common.Abstractions.Storage;
+using Churchee.Module.Events.Entities;
+using Churchee.Module.Events.Specifications;
+using FluentAssertions;
+using Moq;
+
+namespace Churchee.Module.Events.Tests.Specifications
+{
+    public class EventByIdIncludingDatesSpecificationTests
+    {
+        [Fact]
+        public void EventByIdIncludingDatesSpecification_FiltersCorrectly()
+        {
+            var testEvent = new Event(Guid.NewGuid(), Guid.NewGuid(), "/events", Guid.NewGuid(), "", "", "Test Event", "", "", "", "", "", "", "", null, null, DateTime.Now.AddDays(-2), DateTime.Now.AddDays(-1), "");
+            var testEvent2 = new Event(Guid.NewGuid(), Guid.NewGuid(), "/events", Guid.NewGuid(), "", "", "Test Event 2", "", "", "", "", "", "", "", null, null, DateTime.Now.AddDays(-2), DateTime.Now.AddDays(-1), "");
+
+            var events = new List<Event> { testEvent, testEvent2 };
+
+            var mockEventRepository = new Mock<IRepository<Event>>();
+
+            var specification = new EventByIdIncludingDatesSpecification(testEvent.Id);
+
+            var results = specification.Evaluate(events).ToList();
+
+            results.Count.Should().Be(1);
+        }
+    }
+}
