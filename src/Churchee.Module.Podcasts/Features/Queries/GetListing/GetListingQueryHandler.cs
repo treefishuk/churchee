60a8c65ab52fe59fcb3a8fd331847777f42db695
Jon Ryan <jon@treefish.uk>
2024-10-12 23:32:02 +0100

    More Tests. Refactored PodcastListingHandler

--- src/Churchee.Module.Podcasts/Features/Queries/GetListing/GetListingQueryHandler.cs
+++ src/Churchee.Module.Podcasts/Features/Queries/GetListing/GetListingQueryHandler.cs
@@ -1,9 +1,8 @@
 ﻿using Churchee.Common.Abstractions;
 using Churchee.Common.Storage;
 using Churchee.Module.Podcasts.Entities;
+using Churchee.Module.Podcasts.Specifications;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using System.Linq.Dynamic.Core;
 
 namespace Churchee.Module.Podcasts.Features.Queries
 {
@@ -21,22 +20,13 @@ namespace Churchee.Module.Podcasts.Features.Queries
         {
             var repo = _storage.GetRepository<Podcast>();
 
-            int count = repo.Count();
-
-            var query = repo.GetQueryable();
-
-            if (!string.IsNullOrEmpty(request.SearchText))
-            {
-                query = query.Where(w => w.Title.Contains(request.SearchText));
-            }
-
-            string orderby = $"{request.OrderBy} {request.OrderByDirection}";
-
-            var items = await query
-                .OrderBy(orderby)
-                .Skip(request.Skip)
-                .Take(request.Take)
-                .Select(s => new GetListingQueryResponseItem
+            var dataTableResponse = await repo.GetDataTableResponseAsync(
+                specification: new PodcastSearchFilterSpecification(request.SearchText),
+                orderBy: request.OrderBy,
+                orderByDir: request.OrderByDirection,
+                skip: request.Skip,
+                take: request.Take,
+                selector: s => new GetListingQueryResponseItem
                 {
                     Id = s.Id,
                     Active = true,
@@ -44,17 +34,10 @@ namespace Churchee.Module.Podcasts.Features.Queries
                     Title = s.Title,
                     PublishedDate = s.PublishedDate,
                     Source = s.SourceName
-                })
-                .ToListAsync(cancellationToken);
+                },
+                cancellationToken: cancellationToken);
 
-            return new DataTableResponse<GetListingQueryResponseItem>
-            {
-                RecordsTotal = count,
-                RecordsFiltered = count,
-                Draw = request.Take,
-                Data = items
-            };
-
+            return dataTableResponse;
         }
     }
 }
---
+++ test/Churchee.Module.Podcasts.Tests/Features/Queries/GetListing/GetListingQueryHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using Churchee.Common.Abstractions;
+using Churchee.Common.Abstractions.Storage;
+using Churchee.Common.Storage;
+using Churchee.Module.Podcasts.Entities;
+using Churchee.Module.Podcasts.Features.Queries;
+using Churchee.Module.Podcasts.Specifications;
+using FluentAssertions;
+using Moq;
+using System.Linq.Expressions;
+
+namespace Churchee.Module.Podcasts.Tests.Features.Queries.GetListing
+{
+    public class GetListingQueryHandlerTests
+    {
+        private readonly Mock<IDataStore> _dataStoreMock;
+        private readonly Mock<IRepository<Podcast>> _repositoryMock;
+
+        public GetListingQueryHandlerTests()
+        {
+            _dataStoreMock = new Mock<IDataStore>();
+            _repositoryMock = new Mock<IRepository<Podcast>>();
+            _repositoryMock.Setup(s => s.GetDataTableResponseAsync(It.IsAny<PodcastSearchFilterSpecification>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<Expression<Func<Podcast, GetListingQueryResponseItem>>>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new DataTableResponse<GetListingQueryResponseItem>()
+                {
+                    Data =
+                    [
+                        new GetListingQueryResponseItem() { Id = Guid.NewGuid(), Title = "Test", Source = "Test", Active = true },
+                        new GetListingQueryResponseItem() { Id = Guid.NewGuid(), Title = "Test 2", Source = "Test", Active = true}
+                    ]
+                });
+
+
+            _dataStoreMock.Setup(ds => ds.GetRepository<Podcast>()).Returns(_repositoryMock.Object);
+
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnPodcasts()
+        {
+            // Arrange
+            var query = new GetListingQuery(0, 10, string.Empty, "Title asc");
+            var handler = new GetListingQueryHandler(_dataStoreMock.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Data.Count().Should().BeGreaterThan(0);
+        }
+    }
+}
