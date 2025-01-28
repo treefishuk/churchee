20e049988dd8f8c4e4756e7c8f9a4294b5af8258
Jon <jon@treefish.uk>
2025-01-28 18:14:01 +0000

    Added Column Selection and Column Width Persistance

---
+++ src/Churchee.Module.UI/wwwroot/scripts/localStorageHelper.js
@@ -0,0 +1,8 @@
+﻿window.localStorageHelper = {
+    save: function (key, data) {
+        localStorage.setItem(key, JSON.stringify(data));
+    },
+    load: function (key) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+};
