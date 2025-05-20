143cbf846bc7aa4fa2401d78c3e954f886919b2c
Jon <jon@treefish.uk>
2025-05-20 21:29:03 +0100

    Added Delete To Page Type

---
+++ src/Churchee.Module.Site/Features/PageTypes/Commands/DeletePageType/DeletePageTypeCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Churchee.Common.ResponseTypes;
+using Churchee.Common.Storage;
+using Churchee.Module.Site.Entities;
+using MediatR;
+
+namespace Churchee.Module.Site.Features.PageTypes.Commands
+{
+    public class DeletePageTypeCommandHandler : IRequestHandler<DeletePageTypeCommand, CommandResponse>
+    {
+        private readonly IDataStore _storage;
+
+        public DeletePageTypeCommandHandler(IDataStore storage)
+        {
+            _storage = storage;
+        }
+
+        public async Task<CommandResponse> Handle(DeletePageTypeCommand request, CancellationToken cancellationToken)
+        {
+            await _storage.GetRepository<PageType>().SoftDelete(request.Id);
+
+            await _storage.SaveChangesAsync(cancellationToken);
+
+            return new CommandResponse();
+        }
+    }
+}
