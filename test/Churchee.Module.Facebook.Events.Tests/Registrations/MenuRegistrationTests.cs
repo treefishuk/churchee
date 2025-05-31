691066035c560641ccb2f82cc7cc7f061d9de842
Jon Ryan <jon@treefish.uk>
2025-05-31 14:29:37 +0100

    Added additional tests

---
+++ test/Churchee.Module.Facebook.Events.Tests/Registrations/MenuRegistrationTests.cs
@@ -0,0 +1,38 @@
+﻿using Churchee.Module.Facebook.Events.Registrations;
+
+namespace Churchee.Module.Facebook.Events.Tests.Registrations
+{
+    public class MenuRegistrationTests
+    {
+        [Fact]
+        public void MenuItems_ShouldReturnExpectedMenuItems()
+        {
+            // Arrange
+            var menuRegistration = new MenuRegistration();
+
+            // Act
+            var menuItems = menuRegistration.MenuItems;
+
+            // Assert
+            Assert.NotNull(menuItems);
+            Assert.Single(menuItems);
+
+            var mainMenuItem = menuItems.FirstOrDefault();
+
+            Assert.Equal("Integrations", mainMenuItem?.Name);
+            Assert.Equal("/management/integrations", mainMenuItem?.Path);
+            Assert.Equal("integration_instructions", mainMenuItem?.Icon);
+            Assert.Equal(100, mainMenuItem?.Order);
+            Assert.Equal(1, mainMenuItem?.Children.Count);
+
+            var childMenuItem = mainMenuItem?.Children.FirstOrDefault();
+            Assert.Equal("Facebook Events", childMenuItem?.Name);
+            Assert.Equal("/management/integrations/facebook-events", childMenuItem?.Path);
+            Assert.Equal("event", childMenuItem?.Icon);
+            Assert.Equal(1, childMenuItem?.Order);
+            Assert.Equal(0, childMenuItem?.Children.Count);
+
+        }
+    }
+
+}
---
+++ test/Churchee.Module.Facebook.Events.Tests/Specifications/GetEventByFacebookIdSpecificationTests.cs
@@ -0,0 +1,36 @@
+﻿using Churchee.Module.Events.Entities;
+using Churchee.Module.Facebook.Events.Specifications;
+
+namespace Churchee.Module.Facebook.Events.Tests.Specifications
+{
+    public class GetEventByFacebookIdSpecificationTests
+    {
+        [Fact]
+        public void GetEventByFacebookIdSpecification_Should_Return_SingleResult()
+        {
+            var events = new List<Event>
+            {
+                new Event.Builder()
+                    .SetSourceId("12345")
+                    .SetTitle("Title 1")
+                    .Build(),
+                new Event.Builder()
+                    .SetSourceId("234532")
+                    .SetTitle("Title 1")
+                    .Build(),
+                new Event.Builder()
+                    .SetSourceId("112442")
+                    .SetTitle("Title 1")
+                    .Build(),
+            };
+
+            var spec = new GetEventByFacebookIdSpecification("12345");
+
+            // Act
+            var result = spec.Evaluate(events);
+
+            // Assert
+            Assert.Single(result);
+        }
+    }
+}
