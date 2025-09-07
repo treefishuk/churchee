1c52b8b3ce2063f0fc80aecfc333fa8cf650f911
Jon Ryan <jon@treefish.uk>
2025-09-07 21:03:41 +0100

    EntityRegistrationsTests improvements

--- test/Churchee.Module.Videos.Tests/Registrations/EntityRegistrationsTests.cs
+++ test/Churchee.Module.Videos.Tests/Registrations/EntityRegistrationsTests.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Moq;
+﻿using Churchee.Module.Videos.Entities;
+using Churchee.Test.Helpers.Validation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Churchee.Module.Videos.Registrations
 {
@@ -9,11 +10,27 @@ namespace Churchee.Module.Videos.Registrations
         public void RegisterEntities_ConfiguresVideoEntity()
         {
             // Arrange
-            var modelBuilderMock = new Mock<ModelBuilder>(new object[] { new Microsoft.EntityFrameworkCore.Metadata.Conventions.ConventionSet() }) { CallBase = true };
-            var entityRegistrations = new EntityRegistrations();
+            var modelBuilder = GetBuilder();
 
-            // Act & Assert (should not throw)
-            entityRegistrations.RegisterEntities(modelBuilderMock.Object);
+            // Act
+            var EntityTypeVideo = modelBuilder.Model.FindEntityType(typeof(Video));
+
+            // Assert Table Name
+            EntityTypeVideo?.GetTableName()?.Should().Be("Videos");
+
+            // Assert VideoUri has an index
+            EntityTypeVideo?.GetIndexes().Any(idx => idx.Properties.Any(p => p.Name == nameof(Video.VideoUri))).Should().BeTrue();
+        }
+
+        private static ModelBuilder GetBuilder()
+        {
+            // Arrange
+            var modelBuilder = new ModelBuilder(new Microsoft.EntityFrameworkCore.Metadata.Conventions.ConventionSet());
+            var cut = new EntityRegistrations();
+
+            // Act
+            cut.RegisterEntities(modelBuilder);
+            return modelBuilder;
         }
     }
 }
