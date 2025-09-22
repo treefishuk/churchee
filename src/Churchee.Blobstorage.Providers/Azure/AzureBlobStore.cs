1ec879c0f94c1cf4b160998a51d51cfcb680ccc6
Jon Ryan <jon@treefish.uk>
2025-09-22 21:56:58 +0100

    Work on chunk upload

--- src/Churchee.Blobstorage.Providers/Azure/AzureBlobStore.cs
+++ src/Churchee.Blobstorage.Providers/Azure/AzureBlobStore.cs
@@ -16,9 +16,9 @@ namespace Churchee.Blobstorage.Providers.Azure
     {
         private readonly string _connectionString;
 
-        public AzureBlobStore(IConfiguration configuartion)
+        public AzureBlobStore(IConfiguration configuration)
         {
-            _connectionString = configuartion.GetConnectionString("Storage");
+            _connectionString = configuration.GetConnectionString("Storage");
         }
 
         public async Task<Stream> GetReadStreamAsync(Guid applicationTenantId, string fullPath, CancellationToken cancellationToken = default)
@@ -92,9 +92,18 @@ namespace Churchee.Blobstorage.Providers.Azure
         {
             var client = new BlobContainerClient(_connectionString, applicationTenantId.ToString());
 
+            var exists = await client.ExistsAsync(cancellationToken);
+
+            if (!exists)
+            {
+                await client.CreateAsync(cancellationToken: cancellationToken);
+            }
+
             var blobClient = client.GetBlockBlobClient(fullPath);
 
-            using var fileStream = file.OpenReadStream(cancellationToken: cancellationToken);
+            long maxFileSize = 100 * 1024 * 1024; // 100MB
+
+            using var fileStream = file.OpenReadStream(maxAllowedSize: maxFileSize, cancellationToken: cancellationToken);
 
             int blockSize = 1 * 1024 * 1024; // 1MB
             var blockIds = new List<string>();
@@ -106,7 +115,7 @@ namespace Churchee.Blobstorage.Providers.Azure
             while (fileStream.Position < fileStream.Length)
             {
                 byte[] buffer = new byte[blockSize];
-                int bytesRead = await fileStream.ReadAsync(buffer, 0, blockSize);
+                int bytesRead = await fileStream.ReadAsync(buffer.AsMemory(0, blockSize), cancellationToken);
                 string blockId = Convert.ToBase64String(Encoding.UTF8.GetBytes(index.ToString("d6")));
                 using var ms = new MemoryStream(buffer, 0, bytesRead);
                 await blobClient.StageBlockAsync(blockId, ms, cancellationToken: cancellationToken);
