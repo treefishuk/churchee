91e22a9d2150707bd2d6b88f3d6e02f1201019e0
Jon Ryan <jon@treefish.uk>
2025-11-27 08:28:35 +0000

    Work on Tweets

--- src/Churchee.Module.X/Features/Tweets/Commands/EnableTweetsSync/EnableTweetsSyncCommandHandler.cs
+++ src/Churchee.Module.X/Features/Tweets/Commands/EnableTweetsSync/EnableTweetsSyncCommandHandler.cs
@@ -3,16 +3,14 @@ using Churchee.Common.Abstractions.Queue;
 using Churchee.Common.ResponseTypes;
 using Churchee.Common.Storage;
 using Churchee.Module.Site.Entities;
-using Churchee.Module.Site.Specifications;
 using Churchee.Module.Tokens.Entities;
-using Churchee.Module.Tokens.Specifications;
 using Churchee.Module.x.Helpers;
-using Churchee.Module.X.Exceptions;
+using Churchee.Module.X.Jobs;
+using Hangfire;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using System.Net.Http.Headers;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 
 namespace Churchee.Module.X.Features.Tweets.Commands.EnableTweetsSync
 {
@@ -69,10 +67,9 @@ namespace Churchee.Module.X.Features.Tweets.Commands.EnableTweetsSync
 
                 await _dataStore.SaveChangesAsync(cancellationToken);
             }
-
             try
             {
-                _jobService.ScheduleJob($"{applicationTenantId}_SyncTweets", () => SyncTweets(applicationTenantId, CancellationToken.None), () => "*/20 * * * *");
+                _jobService.ScheduleJob<SyncTweets>($"{applicationTenantId}_SyncTweets", a => a.ExecuteAsync(applicationTenantId, CancellationToken.None), Cron.Hourly);
             }
             catch (Exception ex)
             {
@@ -88,7 +85,7 @@ namespace Churchee.Module.X.Features.Tweets.Commands.EnableTweetsSync
 
             try
             {
-                _jobService.QueueJob(() => SyncTweets(applicationTenantId, CancellationToken.None));
+                _jobService.QueueJob<SyncTweets>(x => x.ExecuteAsync(applicationTenantId, CancellationToken.None));
             }
             catch (Exception ex)
             {
@@ -166,83 +163,6 @@ namespace Churchee.Module.X.Features.Tweets.Commands.EnableTweetsSync
 
             return response;
         }
-
-
-        public async Task SyncTweets(Guid applicationTenantId, CancellationToken cancellationToken)
-        {
-            using var httpClient = _httpClientFactory.CreateClient();
-
-            var tokenRepo = _dataStore.GetRepository<Token>();
-
-            string bearerToken = await tokenRepo.FirstOrDefaultAsync(new GetTokenByKeySpecification(SettingKeys.XBearerToken, applicationTenantId), s => s.Value, cancellationToken);
-
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
-
-            string userId = await _settingStore.GetSettingValue(Guid.Parse(SettingKeys.XUserId), applicationTenantId);
-
-            string getTweetsUrl = $"https://api.twitter.com/2/users/{userId}/tweets?tweet.fields=created_at&max_results=10";
-
-            var response = await httpClient.GetAsync(getTweetsUrl, cancellationToken);
-
-            if (response.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
-            {
-                return;
-            }
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new XSyncException($"Failed To Get Tweets. Response code: {response.StatusCode}");
-            }
-
-            string responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
-
-            var deserializedResponse = JsonSerializer.Deserialize<GetTweetsApiResponse>(responseBody);
-
-            if (deserializedResponse == null)
-            {
-                throw new XSyncException("Failed to deserialize tweets from Twitter API response");
-            }
-
-            if (deserializedResponse.Tweets.Count == 0)
-            {
-                return;
-            }
-
-            var mediaItemsRepo = _dataStore.GetRepository<MediaItem>();
-            var mediaFolderRepo = _dataStore.GetRepository<MediaFolder>();
-
-            var tweetsMediaFolder = await mediaFolderRepo.FirstOrDefaultAsync(new MediaFolderByNameSpecification("Tweets", applicationTenantId), cancellationToken);
-
-            foreach (var tweet in deserializedResponse.Tweets)
-            {
-                if (mediaItemsRepo.AnyWithFiltersDisabled(x => x.Title == $"Tweet: {tweet.Id}"))
-                {
-                    continue;
-                }
-
-                string tweetContent = tweet.Text;
-
-                string linkPattern = @"(https?://[^\s]+)"; // Match HTTP/HTTPS URLs
-                string linkReplacement = "<a href=\"$1\">$1</a>";
-
-                tweetContent = Regex.Replace(tweetContent, linkPattern, linkReplacement, RegexOptions.None, TimeSpan.FromMilliseconds(500));
-
-                string handlePattern = @"(?<!\S)@(\w+)"; // Ensure @username is not part of an email address
-                string handleReplacement = "<a href=\"https://x.com/$1\">@$1</a>";
-
-                tweetContent = Regex.Replace(tweetContent, handlePattern, handleReplacement, RegexOptions.None, TimeSpan.FromMilliseconds(500));
-
-                var newTweet = new MediaItem(applicationTenantId, $"Tweet: {tweet.Id}", "/_content/Churchee.Module.X/img/tweet.png", "", tweetContent, tweetsMediaFolder.Id)
-                {
-                    CreatedDate = tweet.CreatedAt
-                };
-
-                mediaItemsRepo.Create(newTweet);
-            }
-
-            await _dataStore.SaveChangesAsync(cancellationToken);
-        }
-
     }
 
 }
