712524996ec9fb9b9afae5e53a754995776c84a5
Jon Ryan <jon@treefish.uk>
2026-01-14 17:58:47 +0000

    New Tests and Minor Refactoring

---
+++ test/Churchee.Infrastructure.AiTools.Tests/Settings/AzureVisionSettingsTests.cs
@@ -0,0 +1,22 @@
+﻿using Churchee.Infrastructure.AiTools.Settings;
+using Churchee.Test.Helpers.Validation;
+
+namespace Churchee.Infrastructure.AiTools.Tests.Settings
+{
+    public class AzureVisionSettingsTests
+    {
+        [Fact]
+        public void AzureVisionSettings_ShouldHaveExpectedProperties()
+        {
+            // Arrange & Act
+            var settings = new AzureVisionSettings
+            {
+                Endpoint = "https://example.com",
+                ApiKey = "test-api-key"
+            };
+            // Assert
+            settings.Endpoint.Should().Be("https://example.com");
+            settings.ApiKey.Should().Be("test-api-key");
+        }
+    }
+}
