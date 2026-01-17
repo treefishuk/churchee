790c49f74635849c85206138ad889d09fffab578
Jon Ryan <jon@treefish.uk>
2026-01-17 20:55:09 +0000

    Test Fix

--- test/Churchee.Module.Site.Tests/Jobs/PublishArticlesJobTests.cs
+++ test/Churchee.Module.Site.Tests/Jobs/PublishArticlesJobTests.cs
@@ -1,4 +1,7 @@
-﻿using Churchee.Common.Storage;
+﻿using Ardalis.Specification;
+using Churchee.Common.Abstractions.Storage;
+using Churchee.Common.Storage;
+using Churchee.Module.Site.Entities;
 using Churchee.Module.Site.Jobs;
 using Moq;
 
@@ -7,12 +10,26 @@ namespace Churchee.Module.Site.Tests.Jobs
     public class PublishArticlesJobTests
     {
         [Fact]
-        public void PublishArticlesJob_Calls_SaveCHangeAsync_OnDbContext()
+        public async Task PublishArticlesJob_Calls_SaveChangeAsync_OnDbContext()
         {
+            // Arrange
             var mockStore = new Mock<IDataStore>();
+
+            var mockRepo = new Mock<IRepository<Article>>();
+
+            mockStore.Setup(s => s.GetRepository<Article>()).Returns(mockRepo.Object);
+
+            mockRepo.Setup(Object => Object.GetListAsync(
+                It.IsAny<ISpecification<Article>>(),
+                It.IsAny<CancellationToken>()
+            )).ReturnsAsync([]);
 
             var cut = new PublishArticlesJob(mockStore.Object);
 
+            // Act
+            await cut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
             mockStore.Verify(v => v.SaveChangesAsync(), Times.Once);
         }
 
