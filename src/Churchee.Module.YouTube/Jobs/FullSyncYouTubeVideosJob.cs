701057d92256ea96dcd2d93d1988c6e1dddce98d
Jon Ryan <jon@treefish.uk>
2026-01-23 20:28:30 +0000

    Build Fix

--- src/Churchee.Module.YouTube/Jobs/FullSyncYouTubeVideosJob.cs
+++ src/Churchee.Module.YouTube/Jobs/FullSyncYouTubeVideosJob.cs
@@ -1,14 +1,12 @@
-﻿using Churchee.Common.Abstractions.Auth;
-using Churchee.Common.Abstractions.Queue;
+﻿using Churchee.Common.Abstractions.Queue;
 using Churchee.Common.Storage;
-using Microsoft.Extensions.Logging;
 
 namespace Churchee.Module.YouTube.Jobs
 {
     public class FullSyncYouTubeVideosJob : SyncYouTubeVideosJobBase, IJob
     {
-        public FullSyncYouTubeVideosJob(ISettingStore settingStore, IDataStore dataStore, ICurrentUser currentUser, IJobService jobService, IHttpClientFactory httpClientFactory, ILogger<FullSyncYouTubeVideosJob> logger)
-            : base(settingStore, dataStore, currentUser, jobService, httpClientFactory, logger)
+        public FullSyncYouTubeVideosJob(ISettingStore settingStore, IDataStore dataStore, IHttpClientFactory httpClientFactory)
+            : base(settingStore, dataStore, httpClientFactory)
         {
         }
 
--- src/Churchee.Module.YouTube/Jobs/SyncYouTubeVideosJob.cs
+++ src/Churchee.Module.YouTube/Jobs/SyncYouTubeVideosJob.cs
@@ -1,14 +1,12 @@
-﻿using Churchee.Common.Abstractions.Auth;
-using Churchee.Common.Abstractions.Queue;
+﻿using Churchee.Common.Abstractions.Queue;
 using Churchee.Common.Storage;
-using Microsoft.Extensions.Logging;
 
 namespace Churchee.Module.YouTube.Jobs
 {
     public class SyncYouTubeVideosJob : SyncYouTubeVideosJobBase, IJob
     {
-        public SyncYouTubeVideosJob(ISettingStore settingStore, IDataStore dataStore, ICurrentUser currentUser, IJobService jobService, IHttpClientFactory httpClientFactory, ILogger<SyncYouTubeVideosJob> logger)
-            : base(settingStore, dataStore, currentUser, jobService, httpClientFactory, logger)
+        public SyncYouTubeVideosJob(ISettingStore settingStore, IDataStore dataStore, IHttpClientFactory httpClientFactory)
+            : base(settingStore, dataStore, httpClientFactory)
         {
         }
 
