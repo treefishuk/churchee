f1dcc0ed05093795f3cfc080b9b18c984ae439c1
Jon Ryan <jon@treefish.uk>
2026-01-30 22:26:39 +0000

    Added some tests

---
+++ test/Churchee.Common.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Churchee.Common.Extensions;
+
+namespace Churchee.Common.Tests.Extensions
+{
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void TruncateToSecondsTest()
+        {
+            // Arrange
+            var dateTime = new DateTime(2024, 1, 1, 1, 1, 34, 22, 22);
+
+            // Act
+            var result = dateTime.TruncateToSeconds();
+
+            // Assert
+            Assert.Equal(new DateTime(2024, 1, 1, 1, 1, 34, 0, 0), result);
+        }
+
+    }
+}
