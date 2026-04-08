d591f45918db2f21ca122ce2549c4f7a3f14b79b
Jon <jon@treefish.uk>
2026-04-08 14:45:59 +0100

    ChurcchSuite Integration Ready

--- src/Churchee.Module.ChurchSuite/Registrations/MenuRegistration.cs
+++ src/Churchee.Module.ChurchSuite/Registrations/MenuRegistration.cs
@@ -12,7 +12,7 @@ namespace Churchee.Module.ChurchSuite.Registrations
                 var list = new List<MenuItem>
                 {
                     new MenuItem("Integrations", "/management/integrations", "integration_instructions", 100)
-                    .AddChild(new MenuItem("Churchsuite Events", "/management/integrations/churchsuite-events", "event"))
+                    .AddChild(new MenuItem("ChurchSuite Events", "/management/integrations/churchsuite-events", "event"))
                 };
 
                 return list;
