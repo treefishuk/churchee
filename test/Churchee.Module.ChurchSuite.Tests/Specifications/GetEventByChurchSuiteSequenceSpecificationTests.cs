4b39993c489b667e3ba9c95068d17896c08e6389
Jon <jon@treefish.uk>
2026-06-17 18:44:51 +0100

    Test Fix

--- test/Churchee.Module.ChurchSuite.Tests/Specifications/GetEventByChurchSuiteSequenceSpecificationTests.cs
+++ test/Churchee.Module.ChurchSuite.Tests/Specifications/GetEventByChurchSuiteSequenceSpecificationTests.cs
@@ -8,23 +8,28 @@ namespace Churchee.Module.Facebook.Events.Tests.Specifications
         [Fact]
         public void GetEventByFacebookIdSpecification_Should_Return_SingleResult()
         {
+            Guid tenantId = Guid.NewGuid();
+
             var events = new List<Event>
             {
                 new Event.Builder()
+                    .SetApplicationTenantId(tenantId)
                     .SetSourceId("12345")
                     .SetTitle("Title 1")
                     .Build(),
                 new Event.Builder()
+                    .SetApplicationTenantId(tenantId)
                     .SetSourceId("234532")
                     .SetTitle("Title 1")
                     .Build(),
                 new Event.Builder()
+                    .SetApplicationTenantId(tenantId)
                     .SetSourceId("112442")
                     .SetTitle("Title 1")
                     .Build(),
             };
 
-            var spec = new GetEventByChurchSuiteSequenceSpecification("12345");
+            var spec = new GetEventByChurchSuiteSequenceSpecification("12345", tenantId);
 
             // Act
             var result = spec.Evaluate(events);
